4a41a4cb7cddc9c4891f0da482f89e7a1c3c554b
Noor Krichen <krichenenour2@gmail.com>
2022-03-10 22:47:24 +0100

    Add push operation button

--- operation-handler-ui/src/App.js
+++ operation-handler-ui/src/App.js
@@ -4,27 +4,49 @@ import axios from "axios";
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
+  const [amountInput, setAmount] = useState("");
+  const [labelInput, setLabel] = useState("");
   useEffect(() => {
     axios
       .get("http://localhost:8081/operations")
       .then((res) => setData(res.data));
   }, []);
 
-  return (
-    <div className="App">
-      <ul>
-        {data.map((operation) => (
-          <Operation key={operation.id} operation={operation} />
-        ))}
-      </ul>
+  const pushOp = () => {
+    const op = {
+      amount: amountInput,
+      label: labelInput,
+      executionDate: new Date(),
+      withdrawal: true,
+    };
+    axios.post("http://localhost:8082/add-operation", op);
+  };
 
-      <input
-        type="text"
-        placeholder="Add amount"
-        onChange={(e) => setSearchInput(e.target.value)}
-      />
-      <p>The new operation : {searchInput}</p>
+  return (
+    <div className="App flex flex-col">
+      <div>
+        <ul>
+          {data.map((operation) => (
+            <Operation key={operation.id} operation={operation} />
+          ))}
+        </ul>
+      </div>
+      <div className="py-8">
+        <input
+          type="text"
+          placeholder="Amount"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Label"
+          onChange={(e) => setLabel(e.target.value)}
+        />
+        <p>The new operation : {amountInput + " " + labelInput}</p>
+      </div>
+      <div>
+        <button onClick={() => pushOp()}>Push</button>
+      </div>
     </div>
   );
 };
--- operation-handler/src/main/java/com/operationhandler/controller/OperationProducerController.java
+++ operation-handler/src/main/java/com/operationhandler/controller/OperationProducerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,6 +16,7 @@ public class OperationProducerController {
     @Autowired
     private OperationProducer topicProducer;
 
+    @CrossOrigin
     @PostMapping(value = "/add-operation")
     public ResponseEntity<String> createOrUpdateAccount(@RequestBody Operation operation) {
         topicProducer.send(operation);
